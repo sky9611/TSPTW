ac3ee89af8a1e07e272755ed7a7771c5a8ecdb8e
sky9611 <collinbeyond@gmail.com>
2017-12-09 19:12:27 +0100

    ga update

--- intellji/src/com/septanome/util/GATSPTW.java
+++ intellji/src/com/septanome/util/GATSPTW.java
@@ -5,8 +5,6 @@ import com.septanome.model.*;
 import java.io.*;
 import java.util.*;
 
-import static com.septanome.util.TSPTW.printList;
-
 public class GATSPTW {
     private PlanLivraison planLivraison = new PlanLivraison();
     private HashMap<Long, Livraison> livraisonsMap;
@@ -14,18 +12,18 @@ public class GATSPTW {
     private Commande commande = new Commande();
     private long idEntrepot;
     private List<Long> idLivraisons = new ArrayList<>();
-    private int populationSize = 20;
+    private int populationSize = 15;
     private HashMap<Integer, List<Long>> population = new HashMap<>();
-    private double pc = 0.8;
-    private double pe = 0.2;
-    private int M = 1000;
-    private int OBC_number;
+    private double pc = 0.75;
+    private double pe = 0.3;
+    private int M = 10000;
+    private int OBC_number = 4;
     private Tournee tournee = new Tournee();
+    private int bestIndex = 0;
 
     public Tournee getTournee() {
         return tournee;
     }
-
 
 
     public GATSPTW(PlanLivraison planLivraison, Commande commande) {
@@ -36,9 +34,8 @@ public class GATSPTW {
         }
         idEntrepot = commande.getEntrepot().getId();
         livraisonsMap = planLivraison.getLivraisonsMap();
-        OBC_number = (int)commande.getListLivraison().size()/2;
-//        pc = 1/livraisonsMap.size();
-//        pe = 1-pc;
+        //OBC_number = (int) commande.getListLivraison().size() / 2;
+        //pe = 1/livraisonsMap.size();
     }
 
     private static <T> List<T> deepCopy(List<T> src) throws IOException, ClassNotFoundException {
@@ -64,8 +61,8 @@ public class GATSPTW {
     }
 
     private boolean isFleasible(List<Long> l) {
-        for (int i=1;i<l.size();i++) {
-            if (isViolated(l,i))
+        for (int i = 1; i < l.size(); i++) {
+            if (isViolated(l, i))
                 return false;
         }
         return true;
@@ -75,9 +72,9 @@ public class GATSPTW {
         //long idStart = l.get(index);
         long idDes = l.get(index);
         //double duree = planLivraison.getCheminsMap().get(idStart).get(idDes).getLongeur()/vitesse;
-        double arrivalTime =  calculeArrivalTime(l)[index];
+        double arrivalTime = calculeArrivalTime(l)[index];
         double b = livraisonsMap.get(idDes).getHeureDeFin();
-        if (arrivalTime>b)
+        if (arrivalTime > b)
             return true;
         return false;
     }
@@ -88,17 +85,17 @@ public class GATSPTW {
         double[] leaveTimes = new double[l.size()];
         arrivalTimes[0] = commande.getHeureDeDepart();
         leaveTimes[0] = commande.getHeureDeDepart();
-        for (int i=1;i<l.size();i++) {
-            long idStart = l.get(i-1);
+        for (int i = 1; i < l.size(); i++) {
+            long idStart = l.get(i - 1);
             double duree = livraisonsMap.get(idStart).getDuree();
             long idDes = l.get(i);
             double longeur = planLivraison.getCheminsMap().get(idStart).get(idDes).getLongeur();
             //System.out.println(idStart+"-->"+idDes+" "+longeur);
-            arrivalTimes[i] = leaveTimes[i-1] + duree + longeur/vitesse;
-            if (arrivalTimes[i]>livraisonsMap.get(idDes).getHeureDeDebut()) {
+            arrivalTimes[i] = leaveTimes[i - 1] + duree + longeur / vitesse;
+            if (arrivalTimes[i] > livraisonsMap.get(idDes).getHeureDeDebut()) {
                 leaveTimes[i] = arrivalTimes[i] + duree;
             } else {
-                leaveTimes[i] = livraisonsMap.get(idDes).getHeureDeDebut()+duree;
+                leaveTimes[i] = livraisonsMap.get(idDes).getHeureDeDebut() + duree;
             }
         }
         return arrivalTimes;
@@ -107,75 +104,103 @@ public class GATSPTW {
     public double Cout(List<Long> l) {
         int length = l.size();
         double[] arrivalTimes = calculeArrivalTime(l);
-        double lastArrivalTime = arrivalTimes[length-1];
-        double lastServiceTime = livraisonsMap.get(l.get(l.size()-1)).getDuree();
-        long lastLivraisonID = l.get(length-1);
+        double lastArrivalTime = arrivalTimes[length - 1];
+        double lastServiceTime = livraisonsMap.get(l.get(l.size() - 1)).getDuree();
+        long lastLivraisonID = l.get(length - 1);
         double lastLeavingTime;
-        if (lastArrivalTime>livraisonsMap.get(lastLivraisonID).getHeureDeDebut()) {
+        if (lastArrivalTime > livraisonsMap.get(lastLivraisonID).getHeureDeDebut()) {
             lastLeavingTime = lastArrivalTime + lastServiceTime;
         } else {
-            lastLeavingTime = livraisonsMap.get(lastLivraisonID).getHeureDeDebut()+lastServiceTime;
+            lastLeavingTime = livraisonsMap.get(lastLivraisonID).getHeureDeDebut() + lastServiceTime;
         }
-        Chemin lastChemin = planLivraison.getCheminsMap().get(l.get(length-1)).get(l.get(0));
+        Chemin lastChemin = planLivraison.getCheminsMap().get(l.get(length - 1)).get(l.get(0));
 
-        return lastLeavingTime+lastChemin.getLongeur()/vitesse;
+        return lastLeavingTime + lastChemin.getLongeur() / vitesse;
+    }
+
+    public List<Integer> generateRandomOrder(int index) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < populationSize; i++) {
+            if (i != index)
+                list.add(i);
+        }
+        Collections.shuffle(list);
+        return list;
     }
 
     public boolean findSolution(int iterMax) throws ClassNotFoundException, IOException {
         initPopulation();
         int iter = 0;
         List<Long> bestOrder = new ArrayList<>();
+        List<Long> best = new ArrayList<>();
+        double bestCout = 100000;
+//        getBestSolution();
         int bestKeepTime = 0;
         while (iter < iterMax) {
-            //System.out.println("iter=" + iter);
+//            System.out.println("iter=" + iter);
             HashMap<Integer, List<Long>> newPopulation = roulette();
-            int n = (int) pc * population.size();
-            for (int i = 0; i < n; i = i + 2) {
-                int index1 = (int) (Math.random() * newPopulation.size());
-                int index2 = (int) (Math.random() * newPopulation.size());
-                List<Long> s1 = newPopulation.get(index1);
-                List<Long> s2 = newPopulation.get(index2);
-                newPopulation.remove(index1);
-                newPopulation.remove(index2);
-                HashMap<Integer, List<Long>> children = orderBasedCrossOver(s1, s2);
-                newPopulation.put(newPopulation.size(), children.get(1));
-                if (newPopulation.size() < population.size())
-                    newPopulation.put(newPopulation.size(), children.get(2));
-            }
-            for (Map.Entry<Integer, List<Long>> entry : newPopulation.entrySet()) {
-                double possibility = Math.random();
-                if (possibility < pe) {
-                    List<Long> mutationSolution = localTwoOpt(entry.getValue());
-                    newPopulation.put(entry.getKey(), mutationSolution);
+            List<Integer> list = generateRandomOrder(bestIndex);
+            for (int i = 0; i < (populationSize - 1) / 2; i++) {
+                int index1 = list.get(2 * i);
+                int index2 = list.get(2 * i + 1);
+                if (Math.random() < pc) {
+                    List<Long> s1 = newPopulation.get(index1);
+                    List<Long> s2 = newPopulation.get(index2);
+                    newPopulation.remove(index1);
+                    newPopulation.remove(index2);
+                    HashMap<Integer, List<Long>> children = orderBasedCrossOver(s1, s2);
+                    if (Math.random() < pe) {
+                        newPopulation.put(index1, localTwoOpt(children.get(1)));
+                    } else {
+                        newPopulation.put(index1, children.get(1));
+                    }
+                    if (Math.random() < pe) {
+                        newPopulation.put(index2, localTwoOpt(children.get(2)));
+                    } else {
+                        newPopulation.put(index2, children.get(2));
+                    }
+//                    newPopulation.put(index1, children.get(1));
+//                    newPopulation.put(index2, children.get(2));
                 }
             }
+//            for (Map.Entry<Integer, List<Long>> entry : newPopulation.entrySet()) {
+//                double possibility = Math.random();
+//                if (possibility < pe) {
+//                    List<Long> mutationSolution = localTwoOpt(entry.getValue());
+//                    newPopulation.put(entry.getKey(), mutationSolution);
+//                }
+//            }
             population = newPopulation;
-            List<Long> newBestOrder = getBestSolution();
-            if (newBestOrder.equals(bestOrder)) bestKeepTime++;
-            else bestKeepTime = 0;
-            bestOrder = newBestOrder;
-
 
             iter++;
             double[] fitnesses = new double[population.size()];
             for (Map.Entry<Integer, List<Long>> entry : population.entrySet()) {
+//                System.out.println(entry.getKey()+" "+entry.getValue());
                 fitnesses[entry.getKey()] = fitness(entry.getValue());
             }
-            if (averageOf(fitnesses) > 0.99999 * maxOf(fitnesses)) break;
-            if (bestKeepTime > 10) break;
+            List<Long> newBestOrder = getBestSolution();
+            if (newBestOrder.equals(bestOrder)) bestKeepTime++;
+            else bestKeepTime = 0;
+            bestOrder = newBestOrder;
+            if (Cout(bestOrder) < bestCout) {
+                best = bestOrder;
+                bestCout = Cout(bestOrder);
+            }
+            //System.out.println(maxOf(fitnesses));
+            if (isFleasible(bestOrder)) {
+                if (averageOf(fitnesses) > 0.99999 * maxOf(fitnesses)) break;
+                if (bestKeepTime > 14) break;
+            }
         }
-
+        bestOrder = best;
         //bestOrder = getBestSolution();
 //        printList(bestOrder);
 //        System.out.println(isFleasible(bestOrder));
-        System.out.println("iter="+iter+" keep time="+bestKeepTime);
+        System.out.println("iter=" + iter + " keep time=" + bestKeepTime);
         System.out.println(Cout(bestOrder));
         //Tournee t = new Tournee();
         List<Chemin> chemins = new ArrayList<>();
-        for (
-                int j = 1; j < bestOrder.size(); j++)
-
-        {
+        for (int j = 1; j < bestOrder.size(); j++) {
             Chemin c = planLivraison.getCheminsMap().get(bestOrder.get(j - 1)).get(bestOrder.get(j));
             chemins.add(c);
         }
@@ -261,16 +286,16 @@ public class GATSPTW {
     }
 
     private HashMap<Integer, List<Long>> orderBasedCrossOver(List<Long> s1, List<Long> s2) {
-        List<Long> child1;
-        List<Long> child2;
-        Long[] child1_array = new Long[s1.size()];
-        Long[] child2_array = new Long[s2.size()];
+        List<Long> child1 = new ArrayList<>();
+        List<Long> child2 = new ArrayList<>();
+        long[] child1_array = new long[s1.size()];
+        long[] child2_array = new long[s2.size()];
         HashMap<Integer, List<Long>> result = new HashMap<>();
         HashSet<Integer> index = new HashSet<Integer>();
         long[] idTosBeCrossed = new long[OBC_number];
         while (index.size() < OBC_number) {
-            index.add((int) Math.random() * s1.size());
-
+            int a = (int) (Math.random() * s1.size());
+            index.add(a);
         }
         Integer[] indexArray_Integer = index.toArray(new Integer[]{});
         int[] indexArray_int = new int[indexArray_Integer.length];
@@ -284,10 +309,16 @@ public class GATSPTW {
         }
         int n = 0;
         for (int k = 0; k < child1_array.length; k++) {
-            if (child1_array[k] == 0) child1_array[k] = idTosBeCrossed[n];
-            n++;
+            if (child1_array[k] == 0) {
+                child1_array[k] = idTosBeCrossed[n];
+                n++;
+            }
+
         }
-        child1 = Arrays.asList(child1_array);
+
+        for (int i = 0; i < child1_array.length; i++)
+            child1.add(child1_array[i]);
+
         result.put(1, child1);
 
         for (int i = 0; i < indexArray_Integer.length; i++) {
@@ -298,13 +329,19 @@ public class GATSPTW {
                 child2_array[j] = s1.get(j);
         }
         int n2 = 0;
-        for (int k = 0; k < child1_array.length; k++) {
-            if (child2_array[k] == 0) child2_array[k] = idTosBeCrossed[n];
-            n2++;
+        for (int k = 0; k < child2_array.length; k++) {
+            if (child2_array[k] == 0) {
+                child2_array[k] = idTosBeCrossed[n2];
+                n2++;
+            }
+
         }
-        child2 = Arrays.asList(child2_array);
+//        child2 = Arrays.asList(child2_array);
+        for (int i = 0; i < child2_array.length; i++)
+            child2.add(child2_array[i]);
         result.put(2, child2);
         return result;
+
     }
 
     private boolean contains(long[] a, long id) {
@@ -323,6 +360,18 @@ public class GATSPTW {
                     return l2;
             }
         }
+//        List<Long> l2 = deepCopy(l);
+//        int i = (int) (Math.random() * l.size());
+//        int j = (int) (Math.random() * l.size());
+//        List<Long> reverse;
+//        if (i < j) {
+//            reverse = l2.subList(i, j);
+//        } else{
+//            reverse = l2.subList(j, i);
+//        }
+//
+//        Collections.reverse(reverse);
+//        return l2;
 
         return l;
     }
@@ -338,7 +387,7 @@ public class GATSPTW {
             }
             ;
         }
-
+        bestIndex = index;
         return population.get(index);
     }
 
